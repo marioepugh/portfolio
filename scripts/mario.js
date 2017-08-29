1dfe635358515a38b1662530f3ae85b0987c968e
marioepugh <marioepugh@gmail.com>
2017-08-29 08:39:05 -0700

    numerous changes

---
+++ scripts/mario.js
@@ -0,0 +1,61 @@
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+$('#showSlides')
+
+$('#home').show();
+
+function showHome() {
+  $('section.section').hide();
+  $('#home').show();
+  $('#about').show();
+  $('#projects').show();
+  $('#skills').show();
+}
+
+function showAbout() {
+  $('section.section').hide();
+  $('#about').show();
+}
+
+function showProjects() {
+  $('section.section').hide();
+  $('#projects').show();
+}
+
+function showSkills() {
+  $('section.section').hide();
+  $('#skills').show();
+}
+
+$('#home-button').on('click', showHome);
+
+$('#about-button').on('click', showAbout);
+
+$('#projects-button').on('click', showProjects);
+
+$('#skills-button').on('click', showSkills);
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
