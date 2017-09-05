0a650797b5af7223aaef71a52d23f1f37fdc1f43
marioepugh <marioepugh@gmail.com>
2017-09-05 08:50:58 -0700

    deployment 14

---
+++ scripts/repo.js
@@ -0,0 +1,19 @@
+// 'use strict';
+// var app = app || {};
+//
+// (function(module) {
+//   const repos = {};
+//
+//   repos.all = [];
+//
+//   repos.requestRepos = function(callback) {
+// $.get(‘/github/user/repos’)
+// .then(data => data.forEach(function(ele) {repos.all.push(ele)}),
+// err => console.error(err))
+// .then(callback);
+// };
+//
+//   repos.with = attr => repos.all.filter(repo => repo[attr]);
+//
+//   module.repos = repos;
+// })(app);
